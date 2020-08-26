bbaeaf8e654650d05025c2ef5441a0d52f35a5d5
Filipe Canatto <canatto.filipe@gmail.com>
2020-08-26 20:42:07 -0300

    -instalar-configurar-prettier

--- Modulo1/src/lib/queryString.spec.js
+++ Modulo1/src/lib/queryString.spec.js
@@ -1,16 +1,14 @@
-const { queryString } = require('./queryString')
+const { queryString } = require("./queryString");
 
-describe('Object to query string', () => {
-    it('should create a valid query string when an object is provided', () => {
-        const obj = {
-            name: "Filipe",
-            profession: "developer"
-        }
+describe("Object to query string", () => {
+  it("should create a valid query string when an object is provided", () => {
+    const obj = {
+      name: "Filipe",
+      profession: "developer",
+    };
 
-        expect(queryString(obj)).toBe(
-            'name=Filipe&profession=developer'
-        );
-    });
+    expect(queryString(obj)).toBe("name=Filipe&profession=developer");
+  });
 });
 
 // describe('Query string to object', () => {
