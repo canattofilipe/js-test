671c0e0dea7eb357d83527c484a691f6d92b5fd1
Filipe Canatto <canatto.filipe@gmail.com>
2020-08-28 15:42:40 -0300

    mais-casos-teste-querystring

--- Modulo1/src/lib/queryString.spec.js
+++ Modulo1/src/lib/queryString.spec.js
@@ -1,13 +1,36 @@
-const { queryString } = require("./queryString");
+const { queryString } = require('./queryString');
 
-describe("Object to query string", () => {
-  it("should create a valid query string when an object is provided", () => {
+describe('Object to query string', () => {
+  it('should create a valid query string when an object is provided', () => {
     const obj = {
-      name: "Filipe",
-      profession: "developer",
+      name: 'Filipe',
+      profession: 'developer',
     };
 
-    expect(queryString(obj)).toBe("name=Filipe&profession=developer");
+    expect(queryString(obj)).toBe('name=Filipe&profession=developer');
+  });
+
+  it('should create a valid query string even when an array is passed as value', () => {
+    const obj = {
+      name: 'Filipe',
+      abilities: ['JS', 'TDD'],
+    };
+
+    expect(queryString(obj)).toBe('name=Filipe&abilities=JS,TDD');
+  });
+
+  it('should throw an error when an object is passed as a value', () => {
+    const obj = {
+      name: 'Filipe',
+      abilities: {
+        first: 'JS',
+        second: 'TDD',
+      },
+    };
+
+    expect(() => {
+      queryString(obj);
+    }).toThrowError();
   });
 });
 
