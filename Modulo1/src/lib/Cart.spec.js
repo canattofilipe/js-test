44416d53959a01b85e5555083cbda4d989fa251f
Filipe Canatto <canatto.filipe@gmail.com>
2020-09-05 15:30:36 -0300

    assegurar-que-um-o-valor-total-seja-atualizado-quando-produto-é-removido

--- Modulo1/src/lib/Cart.spec.js
+++ Modulo1/src/lib/Cart.spec.js
@@ -6,6 +6,10 @@ describe('Cart', () => {
     title: 'Adidas running shoes - men',
     price: 35388, //353.88 | R$ 383,88
   };
+  let product2 = {
+    title: 'Adidas running shoes - women',
+    price: 41872, //418.72 | R$ 418.72
+  };
 
   beforeEach(() => {
     cart = new Cart();
@@ -38,5 +42,21 @@ describe('Cart', () => {
     });
 
     expect(cart.getTotal()).toEqual(35388);
+  });
+
+  it('should update total when a product gets included and then removed', () => {
+    cart.add({
+      product,
+      quantity: 2,
+    });
+
+    cart.add({
+      product: product2,
+      quantity: 1,
+    });
+
+    cart.remove(product);
+
+    expect(cart.getTotal()).toEqual(41872);
   });
 });
