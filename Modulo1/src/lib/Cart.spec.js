f9005d3b200ca2b5e43709f4a88d9ced374ed10c
Filipe Canatto <canatto.filipe@gmail.com>
2020-09-11 12:09:34 -0300

    segunda-condição-de-desconto-segundo-caso-de-uso e -duas-condições-de-desconto-que-sejam-concorrentes-a-mais-vantajosa-deve-prevalecer

--- Modulo1/src/lib/Cart.spec.js
+++ Modulo1/src/lib/Cart.spec.js
@@ -119,19 +119,81 @@ describe('Cart', () => {
 
       expect(cart.getTotal().getAmount()).toEqual(74315);
     });
-  });
 
-  it('should apply quantity discount for even quantities', () => {
-    const condition = {
-      quantity: 2,
-    };
+    it('should NOT apply percentage discount is below or equals minimum', () => {
+      const condition = {
+        percentage: 30,
+        minimum: 2,
+      };
 
-    cart.add({
-      product,
-      condition,
-      quantity: 4,
+      cart.add({
+        product,
+        condition,
+        quantity: 2,
+      });
+
+      expect(cart.getTotal().getAmount()).toEqual(70776);
     });
 
-    expect(cart.getTotal().getAmount()).toEqual(70776);
+    it('should apply quantity discount for even quantities', () => {
+      const condition = {
+        quantity: 2,
+      };
+
+      cart.add({
+        product,
+        condition,
+        quantity: 4,
+      });
+
+      expect(cart.getTotal().getAmount()).toEqual(70776);
+    });
+
+    it('should NOT apply quantity discount for even quantities when condition is not met', () => {
+      const condition = {
+        quantity: 2,
+      };
+
+      cart.add({
+        product,
+        condition,
+        quantity: 4,
+      });
+
+      expect(cart.getTotal().getAmount()).toEqual(70776);
+    });
+
+    it('should apply quantity discount for odd quantities', () => {
+      const condition = {
+        quantity: 2,
+      };
+
+      cart.add({
+        product,
+        condition,
+        quantity: 5,
+      });
+
+      expect(cart.getTotal().getAmount()).toEqual(106164);
+    });
+
+    it('should receive two or more conditions and determine/apply the best discount. First case ', () => {
+      const condition1 = {
+        percentage: 30,
+        minimum: 2,
+      };
+
+      const condition2 = {
+        quantity: 2,
+      };
+
+      cart.add({
+        product,
+        condition: [condition1, condition2],
+        quantity: 5,
+      });
+
+      expect(cart.getTotal().getAmount()).toEqual(106164);
+    });
   });
 });
