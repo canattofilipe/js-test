edb56794c164d670ac5ea9c1afd573e93bb4395b
Filipe Canatto <canatto.filipe@gmail.com>
2020-09-11 14:05:01 -0300

    retornar-o-valor-total-formatado-em-reais

--- Modulo1/src/lib/Cart.js
+++ Modulo1/src/lib/Cart.js
@@ -2,21 +2,21 @@ import find from 'lodash/find';
 import remove from 'lodash/remove';
 import Dinero from 'dinero.js';
 
-const calculatePercentageDiscount = (amount, item) => {
+const calculatePercentageDiscount = (amount, { condition, quantity }) => {
   if (
     // optinal chaining operator
-    item.condition?.percentage &&
-    item.quantity > item.condition.minimum
+    condition?.percentage &&
+    quantity > condition.minimum
   ) {
-    return amount.percentage(item.condition.percentage);
+    return amount.percentage(condition.percentage);
   }
 
   return Money({ amount: 0 });
 };
 
-const calculateQuantityDiscount = (amount, item) => {
-  const isEven = item.quantity % 2 === 0;
-  if (item.condition?.quantity && item.quantity > item.condition.quantity) {
+const calculateQuantityDiscount = (amount, { condition, quantity }) => {
+  const isEven = quantity % 2 === 0;
+  if (condition?.quantity && quantity > condition.quantity) {
     return amount.percentage(isEven ? 50 : 40);
   }
 
@@ -67,30 +67,26 @@ export default class Cart {
   }
 
   getTotal() {
-    return this.items.reduce((acc, item) => {
-      const amount = Money({ amount: item.quantity * item.product.price });
+    return this.items.reduce((acc, { quantity, product, condition }) => {
+      const amount = Money({ amount: quantity * product.price });
       let discount = Money({ amount: 0 });
 
-      if (item.condition) {
-        discount = calculateDiscount(amount, item.quantity, item.condition);
+      if (condition) {
+        discount = calculateDiscount(amount, quantity, condition);
       }
-
-      // if (item.condition?.percentage) {
-      //   discount = calculatePercentageDiscount(amount, item);
-      // } else if (item.condition?.quantity) {
-      //   discount = calculateQuantityDiscount(amount, item);
-      // }
 
       return acc.add(amount).subtract(discount);
     }, Money({ amount: 0 }));
   }
 
   sumary() {
-    const total = this.getTotal().getAmount();
+    const total = this.getTotal();
+    const formatted = total.toFormat('$0,0.00');
     const items = this.items;
 
     return {
-      total,
+      total: total.getAmount(),
+      formatted,
       items,
     };
   }
